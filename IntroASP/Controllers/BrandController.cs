ff194b1999287efdac5d3186e6b9a46c0b16564e
Carlos Perez <per.ckarlos@gmail.com>
2023-05-13 18:44:53 -0500

    adding project base

---
+++ IntroASP/Controllers/BrandController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntroASP.Controllers
+{
+    public class BrandController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IntroASP/Models/Brand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntroASP.Models;
+
+public partial class Brand
+{
+    public Brand() 
+    { 
+        Beers = new HashSet<Beer>();
+    }
+    public int BrandId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Beer> Beers { get; set; } = new List<Beer>();
+}
